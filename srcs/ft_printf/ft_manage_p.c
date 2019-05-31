5fbbb7bc3d3636864f3fb5c194d364b356d1e43a
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-05-31 18:07:29 +0200

    norm error fixed

--- srcs/ft_printf/ft_manage_p.c
+++ srcs/ft_printf/ft_manage_p.c
@@ -6,58 +6,11 @@
 /*   By: efischer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 12:04:14 by efischer          #+#    #+#             */
-/*   Updated: 2019/04/19 16:01:02 by efischer         ###   ########.fr       */
+/*   Updated: 2019/05/31 18:04:57 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-/*static char		*ft_p_precision(char *s, t_flag *flag)
-{
-	int		len;
-	char	*tmp;
-
-	if ((len = flag->precision - ft_strlen(s)) > 0)
-	{
-		if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
-			return (NULL);
-		tmp[len--] = '\0';
-		while (len >= 0)
-			tmp[len--] = '0';
-		if (flag->min)
-			s = ft_join_free(s, tmp, 3);
-		else
-			s = ft_join_free(tmp, s, 3);
-	}
-	return (s);
-}
-
-static char		*ft_p_width(void *p, char *s, t_flag *flag)
-{
-	int		len;
-	char	*tmp;
-	char	c;
-
-	if ((len = flag->width - ft_strlen(s)) > 0)
-	{
-		if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
-			return (NULL);
-		c = ' ';
-		if (p == 0 && flag->zero)
-		{
-			c = '0';
-			flag->min = 1;
-		}
-		tmp[len--] = '\0';
-		while (len >= 0)
-			tmp[len--] = c;
-		if (flag->min)
-			s = ft_join_free(s, tmp, 3);
-		else
-			s = ft_join_free(tmp, s, 3);
-	}
-	return (s);
-}*/
 
 char			*ft_manage_p(va_list *arg, t_flag *flag)
 {
