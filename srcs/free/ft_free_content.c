f5a3aeb1f1f2373c6cbdc59eda09495eb567f986
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-05-31 17:59:05 +0200

    add srcs/free/ft_free_content.c

--- srcs/free/ft_free_content.c
+++ srcs/free/ft_free_content.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free.h                                             :+:      :+:    :+:   */
+/*   ft_free_content.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efischer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/18 17:50:12 by efischer          #+#    #+#             */
-/*   Updated: 2019/05/31 17:55:48 by efischer         ###   ########.fr       */
+/*   Created: 2019/05/31 17:54:43 by efischer          #+#    #+#             */
+/*   Updated: 2019/05/31 17:57:05 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FREE_H
-# define FREE_H
+#include "libft.h"
 
-void	ft_free_tab(char **tmp);
-void	ft_free_content(void *content, size_t content_size);
-
-#endif
+void		ft_free_content(void *content, size_t content_size)
+{
+	(void)content_size;
+	ft_strdel((char**)&content);
+}
