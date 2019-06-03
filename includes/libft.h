8ba23db0e7796d64e98c2e974c41cf145bc74acb
MilouFischer <etienne.fischer77@gmail.com>
2019-06-03 15:27:11 +0200

    remove debug putendl

--- includes/libft.h
+++ includes/libft.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LIBFT_H
-# define FT_LIBFT_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <stdlib.h>
 # include <unistd.h>
--- includes/process_str.h
+++ includes/process_str.h
@@ -13,31 +13,31 @@
 #ifndef PROCESS_STR_H
 # define PROCESS_STR_H
 
-char				*ft_join_free(char *s1, char *s2, int op);
-char				*ft_strcapitalize(char *str);
-char				*ft_strcat(char *s1, const char *s2);
-void				ft_strclr(char *s);
-char				*ft_strcpy(char *dest, const char *src);
-void				ft_strdel(char **as);
-char				*ft_strdup(const char *s1);
-void				ft_striter(char *s, void (*f)(char *));
-void				ft_striteri(char *s, void (*f)(unsigned int, char *));
-char				*ft_strjoin(char const *s1, char const *s2);
-size_t				ft_strlcat(char *dest, const char *src, size_t size);
-char				*ft_strlowcase(char *str);
-char				*ft_strmap(char const *s, char (*f)(char));
-char				*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-char				*ft_strncat(char *s1, const char *s2, size_t n);
-char				*ft_strncpy(char *dest, const char *src, size_t len);
-char				*ft_strndup(const char *s1, size_t n);
-char				*ft_strndup_free(char *s1, size_t n);
-char				*ft_strnew(size_t size);
-char				**ft_strsplit(char const *s, char c);
-char				*ft_strsub(char const *s, unsigned int start, size_t len);
-char				*ft_strtrim(char const *s);
-char				*ft_strupcase(char *str);
-int					ft_tolower(int c);
-int					ft_toupper(int c);
-int					get_next_line(const int fd, char **line);
+char	*ft_join_free(char *s1, char *s2, int op);
+char	*ft_strcapitalize(char *str);
+char	*ft_strcat(char *s1, const char *s2);
+void	ft_strclr(char *s);
+char	*ft_strcpy(char *dest, const char *src);
+void	ft_strdel(char **as);
+char	*ft_strdup(const char *s1);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strlowcase(char *str);
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *dest, const char *src, size_t len);
+char	*ft_strndup(const char *s1, size_t n);
+char	*ft_strndup_free(char *s1, size_t n);
+char	*ft_strnew(size_t size);
+char	**ft_strsplit(char const *s, char c);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s);
+char	*ft_strupcase(char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		get_next_line(const int fd, char **line);
 
 #endif
