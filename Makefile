89ae875cb2c285d95da118a702e82f1d46c84cd3
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-06-08 14:04:57 +0200

    new srcs/list/ft_lstlen.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: efischer <efischer@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/04 10:22:22 by efischer          #+#    #+#              #
-#    Updated: 2019/06/06 17:53:54 by efischer         ###   ########.fr        #
+#    Updated: 2019/06/08 14:03:26 by efischer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -142,6 +142,7 @@ SRCS += ft_lstdel.c
 SRCS += ft_lstdelone.c
 SRCS += ft_lstfree.c
 SRCS += ft_lstiter.c
+SRCS += ft_lstlen.c
 SRCS += ft_lstmap.c
 SRCS += ft_lstnew.c
 SRCS += ft_lstnew_str.c
--- includes/list.h
+++ includes/list.h
@@ -6,7 +6,7 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:15:24 by efischer          #+#    #+#             */
-/*   Updated: 2019/06/06 15:32:22 by efischer         ###   ########.fr       */
+/*   Updated: 2019/06/08 14:03:03 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void		ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void		ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void		ft_lstfree(t_list *list);
 void		ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+size_t		ft_lstlen(t_list *lst);
 t_list		*ft_lstnew(void const *content, size_t content_size);
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 t_list		*ft_lstnew_str(char const *content, size_t content_size);
