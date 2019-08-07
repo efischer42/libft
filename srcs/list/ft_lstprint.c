42e7558ba84d6cdeda24aa8beb50673f94cefa01
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-08-07 16:40:39 +0200

    ft_lstprint: norm OK

--- srcs/list/ft_lstprint.c
+++ srcs/list/ft_lstprint.c
@@ -6,38 +6,47 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:22:03 by efischer          #+#    #+#             */
-/*   Updated: 2019/08/07 11:38:54 by efischer         ###   ########.fr       */
+/*   Updated: 2019/08/07 15:25:51 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_lstprint(t_list *lst, void (*f)(t_list*, t_list**))
+static void	*get_str_to_print(ssize_t *len, t_list *lst,
+			void (*f)(t_list*, t_list**))
 {
+	t_list	*elem;
 	void	*str;
 	void	*tmp;
-	t_list	*elem;
-	ssize_t	len;
 
-	len = 0;
 	str = NULL;
-	if (lst == NULL)
-		return (SUCCESS);
-	if (f == NULL)
-		return (FAILURE);
 	while (lst != NULL)
 	{
 		tmp = str;
 		f(lst, &elem);
 		if (elem == NULL)
 			break ;
-		str = ft_memjoin(tmp, elem->content, len, elem->content_size);
-		len += elem->content_size;
+		str = ft_memjoin(tmp, elem->content, *len, elem->content_size);
+		*len += elem->content_size;
 		free(tmp);
 		ft_strdel((char**)&elem->content);
 		free(elem);
 		lst = lst->next;
 	}
+	return (str);
+}
+
+int			ft_lstprint(t_list *lst, void (*f)(t_list*, t_list**))
+{
+	void	*str;
+	ssize_t	len;
+
+	len = 0;
+	if (lst == NULL)
+		return (SUCCESS);
+	if (f == NULL)
+		return (FAILURE);
+	str = get_str_to_print(&len, lst, f);
 	if (write(1, str, len) == FAILURE)
 		len = FAILURE;
 	free(str);
