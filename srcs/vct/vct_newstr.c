37e0010cb605c258c68d8239ddb1f86186b7c1c5
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-10-29 12:29:59 +0100

    renamed: vct_new -> vct_newstr & new: vct_new + vct_resize

--- srcs/vct/vct_newstr.c
+++ srcs/vct/vct_newstr.c
@@ -1,27 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vct_new.c                                          :+:      :+:    :+:   */
+/*   vct_newstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/29 12:07:48 by efischer          #+#    #+#             */
-/*   Updated: 2019/10/29 12:22:04 by efischer         ###   ########.fr       */
+/*   Created: 2019/10/29 12:06:54 by efischer          #+#    #+#             */
+/*   Updated: 2019/10/29 12:28:01 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"vctlib.h"
 
-t_vector	*vct_new(void)
+t_vector	*vct_newstr(char *str)
 {
 	t_vector	*vct;
 
 	vct = (t_vector*)malloc(sizeof(t_vector));
 	if (vct == NULL)
-		return (NULL);
-	vct->str = NULL;
-	vct->len = 0;
+		return (vct);
+	vct->len = ft_strlen(str);
 	vct->size = DFL_SIZE;
 	vct->scale = DFL_SCALE;
+	if (vct->len > vct->size)
+		vct_resize(vct);
+	vct->str = (char*)malloc(sizeof(char) * vct->size);
+	if (vct->str != NULL)
+	{
+		vct->str = (char*)ft_memcpy(vct->str, str, vct->len);
+		vct->str[vct->len] = '\0';
+	}
 	return (vct);
 }
--- srcs/vct/vct_resize.c
+++ srcs/vct/vct_resize.c
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vctlib.h                                           :+:      :+:    :+:   */
+/*   vct_resize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/26 12:13:44 by efischer          #+#    #+#             */
-/*   Updated: 2019/10/29 12:26:36 by efischer         ###   ########.fr       */
+/*   Created: 2019/10/29 12:23:20 by efischer          #+#    #+#             */
+/*   Updated: 2019/10/29 12:25:58 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VCTLIB_H
-# define VCTLIB_H
+#include	"vctlib.h"
 
-# include "libft.h"
-
-# define	DFL_SIZE 64
-# define	DFL_SCALE 16
-
-typedef struct	s_vector
+void	vct_resize(t_vector	*vct)
 {
-	char	*str;
-	size_t	len;
-	size_t	size;
-	size_t	scale;
-}				t_vector;
+	size_t		overflow;
+	size_t		align;
 
-t_vector	*vct_newstr(char *str);
-t_vector	*vct_new(void);
-void		vct_resize(t_vector	*vct);
-
-#endif
+	overflow = vct->len - vct->size;
+	align = overflow - overflow % DFL_SCALE;
+	vct->scale = 2 * align;
+	vct->size += vct->scale;
+}
