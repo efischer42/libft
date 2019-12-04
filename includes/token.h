5320a83971ed54077e3a067c4a050b6dc14fd1d3
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-12-04 16:51:49 +0100

    remove del_list.c

--- includes/token.h
+++ includes/token.h
@@ -6,7 +6,7 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 10:11:03 by efischer          #+#    #+#             */
-/*   Updated: 2019/12/02 16:48:17 by efischer         ###   ########.fr       */
+/*   Updated: 2019/12/04 16:43:34 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,5 @@ enum	e_token
 int		lexer(const char* str, t_list **lst);
 int		parser(t_list *lst);
 void	debug(t_list *lst);
-void	del_list(t_list *job_lst);
 
 #endif
