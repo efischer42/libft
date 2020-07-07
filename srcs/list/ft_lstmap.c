b6339f5099a875f5731dea0c0d45803a0777b31e
efischer <efischer@student.42.fr>
2020-07-07 12:15:11 +0200

    srcs/list/ft_lstmap.c refactored to fix a leak

--- srcs/list/ft_lstmap.c
+++ srcs/list/ft_lstmap.c
@@ -6,39 +6,36 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:21:51 by efischer          #+#    #+#             */
-/*   Updated: 2019/06/06 15:21:53 by efischer         ###   ########.fr       */
+/*   Updated: 2020/07/07 12:13:09 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void		ft_del(void *content, size_t content_size)
+static void	del(void *content, size_t content_size)
 {
 	(void)content_size;
 	free(content);
 }
 
-t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*new;
-	t_list	*tmp;
+	t_list	*map_lst;
+	t_list	*lst_new;
 
-	if (!lst || !f)
+	if (lst == NULL || f == NULL)
 		return (NULL);
-	if (!(new = (t_list*)malloc(sizeof(t_list))))
-		return (NULL);
-	new = f(lst);
-	tmp = new;
-	while (lst->next)
+	map_lst = NULL;
+	while (lst != NULL)
 	{
-		lst = lst->next;
-		if (!(new->next = (t_list*)malloc(sizeof(t_list))))
+		lst_new = ft_lstnew(f(lst), sizeof(*lst));
+		if (lst_new == NULL)
 		{
-			ft_lstdel(&tmp, ft_del);
+			ft_lstdel(&map_lst, del);
 			return (NULL);
 		}
-		new->next = f(lst);
-		new = new->next;
+		ft_lstaddend(&map_lst, lst_new);
+		lst = lst->next;
 	}
-	return (tmp);
+	return (map_lst);
 }
